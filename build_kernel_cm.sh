12736d759a1446a7e74667204553ea0d36b320f1
FrancescoCG <frank-97@hotmail.gr>
2015-10-28 21:50:49 +0200

    some changes

--- build_kernel_cm.sh
+++ build_kernel_cm.sh
@@ -5,13 +5,13 @@ export PARENT_DIR=`readlink -f ..`
 export USE_SEC_FIPS_MODE=true
 export CROSS_COMPILE=/home/francesco/arm-eabi-6.0/bin/arm-eabi-
 
-# if [ "v7" != "" ];then
-#  export KERNELDIR=`readlink -f v7`
+# if [ "v9" != "" ];then
+#  export KERNELDIR=`readlink -f v9`
 # fi
 
 RAMFS_TMP="/home/francesco/Crazy-Kernel/Kernel_cm/tmp_cm/ramfs"
 
-VER="\"-CrazySuperKernel_CM-v7\""
+VER="\"-CrazySuperKernel_CM-v9\""
 cp -f /home/francesco/Crazy-Kernel/Kernel_cm/arch/arm/configs/0crazykernel1_CM_defconfig /home/francesco/Crazy-Kernel/0crazykernel1_CM_defconfig
 sed "s#^CONFIG_LOCALVERSION=.*#CONFIG_LOCALVERSION=$VER#" /home/francesco/Crazy-Kernel/0crazykernel1_CM_defconfig > /home/francesco/Crazy-Kernel/Kernel_cm/arch/arm/configs/0crazykernel1_CM_defconfig
 
@@ -24,7 +24,7 @@ export KCONFIG_NOTIMESTAMP=true
 export ARCH=arm
 
 cd $KERNELDIR/
-make -j9 || exit 1
+make -j5 || exit 1
 
 #remove previous ramfs files
 rm -rf $RAMFS_TMP
@@ -60,11 +60,11 @@ tools/mkbootimg --cmdline 'console=null androidboot.hardware=qcom user_debug=23 
 cd /home/francesco/Crazy-Kernel
 mv -f -v /home/francesco/Crazy-Kernel/Kernel_cm/boot.img /home/francesco/Crazy-Kernel/G900F_CrazySuperKernel_CM.CWM/boot.img
 cd /home/francesco/Crazy-Kernel/G900F_CrazySuperKernel_CM.CWM
-zip -r ../CrazySuperKernel_CM_v7_CWM.zip .
+zip -r ../CrazySuperKernel_CM_v9_CWM.zip .
 
-adb push /home/francesco/Crazy-Kernel/CrazySuperKernel_CM_v7_CWM.zip /storage/sdcard1/CrazySuperKernel_CM_${1}_CWM.zip
+adb push /home/francesco/Crazy-Kernel/CrazySuperKernel_CM_v9_CWM.zip /storage/sdcard1/CrazySuperKernel_CM_${1}_CWM.zip
 
-# adb push /home/francesco/Crazy-Kernel/CrazySuperKernel_CM_v7_CWM.zip /storage/sdcard1/update-crazysuperkernel.zip
+# adb push /home/francesco/Crazy-Kernel/CrazySuperKernel_CM_v9_CWM.zip /storage/sdcard1/update-crazysuperkernel.zip
 # 
 # adb shell su -c "echo 'boot-recovery ' > /cache/recovery/command"
 # adb shell su -c "echo '--update_package=/storage/sdcard0/update-crazysuperkernel.zip' >> /cache/recovery/command"
